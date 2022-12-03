0ac444520eeaac9b834403fbd2bd8d98fd3e31ac
DESKTOP-PCPA7E5\tuann <tuanngoc06102000@gmail.com>
2022-12-03 12:25:21 +0700

    Add more loss function

---
+++ model/loss.py
@@ -0,0 +1,308 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.autograd
+from torch.nn import Parameter
+import math
+
+
+class ContrastiveLoss(nn.Module):
+    def __init__(self, margin=0.3, **kwargs):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, inputs, targets):
+        n = inputs.size(0)
+        # Compute similarity matrix
+        sim_mat = torch.matmul(inputs, inputs.t())
+        targets = targets
+        loss = list()
+        c = 0
+
+        for i in range(n):
+            pos_pair_ = torch.masked_select(sim_mat[i], targets == targets[i])
+
+            #  move itself
+            pos_pair_ = torch.masked_select(pos_pair_, pos_pair_ < 1)
+            neg_pair_ = torch.masked_select(sim_mat[i], targets != targets[i])
+
+            pos_pair_ = torch.sort(pos_pair_)[0]
+            neg_pair_ = torch.sort(neg_pair_)[0]
+
+            neg_pair = torch.masked_select(neg_pair_, neg_pair_ > self.margin)
+
+            neg_loss = 0
+
+            pos_loss = torch.sum(-pos_pair_ + 1)
+            if len(neg_pair) > 0:
+                neg_loss = torch.sum(neg_pair)
+            loss.append(pos_loss + neg_loss)
+
+        loss = sum(loss) / n
+        return loss
+
+class CircleLoss(nn.Module):
+    def __init__(self, m: float, gamma: float) -> None:
+        super(CircleLoss, self).__init__()
+        self.margin = m
+        self.gamma = gamma
+        self.soft_plus = nn.Softplus()
+        # self.class_num = class_num
+        # self.emdsize = emdsize
+
+        # self.weight = nn.Parameter(torch.FloatTensor(self.class_num, self.emdsize))
+        # nn.init.xavier_uniform_(self.weight)
+        self.use_cuda = True
+
+
+    def forward(self, input, label):
+        # similarity_matrix = nn.functional.linear(nn.functional.normalize(input,p=2, dim=1, eps=1e-12), nn.functional.normalize(self.weight,p=2, dim=1, eps=1e-12))
+        similarity_matrix = input.float()
+        if self.use_cuda:
+            one_hot = torch.zeros(similarity_matrix.size(), device='cuda')
+        else:
+            one_hot = torch.zeros(similarity_matrix.size())
+        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+        one_hot = one_hot.type(dtype=torch.bool)
+        #sp = torch.gather(similarity_matrix, dim=1, index=label.unsqueeze(1))
+        sp = similarity_matrix[one_hot]
+        mask = one_hot.logical_not()
+        sn = similarity_matrix[mask]
+
+        sp = sp.view(input.size()[0], -1)
+        sn = sn.view(input.size()[0], -1)
+
+        ap = torch.clamp_min(-sp.detach() + 1 + self.margin, min=0.)
+        an = torch.clamp_min(sn.detach() + self.margin, min=0.)
+
+        delta_p = 1 - self.margin
+        delta_n = self.margin
+
+        logit_p = - ap * (sp - delta_p) * self.gamma
+        logit_n = an * (sn - delta_n) * self.gamma
+
+        loss = self.soft_plus(torch.logsumexp(logit_n, dim=1) + torch.logsumexp(logit_p, dim=1))
+
+        return loss.mean()
+
+
+
+class Arcface(nn.Module):
+    r"""Implement of large margin arc distance: :
+        Args:
+            in_features: size of each input sample
+            out_features: size of each output sample
+            s: norm of input feature
+            m: margin
+            cos(theta + m)
+        """
+    def __init__(self, in_features, out_features, s=30.0, m=0.30, easy_margin=False, ls_eps=0.0):
+        super(Arcface, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.s = s
+        self.m = m
+        self.ls_eps = ls_eps  # label smoothing
+        self.weight = Parameter(torch.FloatTensor(out_features, in_features))
+        nn.init.xavier_uniform_(self.weight)
+
+        self.easy_margin = easy_margin
+        self.cos_m = math.cos(m)
+        self.sin_m = math.sin(m)
+        self.th = math.cos(math.pi - m)
+        self.mm = math.sin(math.pi - m) * m
+
+    def forward(self, input, label):
+        # --------------------------- cos(theta) & phi(theta) ---------------------------
+        cosine = F.linear(F.normalize(input), F.normalize(self.weight))
+        sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
+        phi = cosine * self.cos_m - sine * self.sin_m
+        phi = phi.type_as(cosine)
+        if self.easy_margin:
+            phi = torch.where(cosine > 0, phi, cosine)
+        else:
+            phi = torch.where(cosine > self.th, phi, cosine - self.mm)
+        # --------------------------- convert label to one-hot ---------------------------
+        # one_hot = torch.zeros(cosine.size(), requires_grad=True, device='cuda')
+        one_hot = torch.zeros(cosine.size(), device='cuda')
+        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+        if self.ls_eps > 0:
+            one_hot = (1 - self.ls_eps) * one_hot + self.ls_eps / self.out_features
+        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        output *= self.s
+
+        return output
+
+class CrossEntropyLossWithLabelSmoothing(nn.Module):
+    def __init__(self, n_dim, ls_=0.9):
+        super().__init__()
+        self.n_dim = n_dim
+        self.ls_ = ls_
+
+    def forward(self, x, target, convert_one_hot=True):
+        if convert_one_hot:
+            target = F.one_hot(target, self.n_dim).float()
+        target *= self.ls_
+        target += (1 - self.ls_) / self.n_dim
+
+        logprobs = torch.nn.functional.log_softmax(x, dim=-1)
+        loss = -logprobs * target
+        loss = loss.sum(-1)
+        return loss.mean()
+
+class LabelSmoothingCrossEntropy(nn.Module):
+    """
+    NLL loss with label smoothing.
+    """
+    def __init__(self, smoothing=0.1):
+        """
+        Constructor for the LabelSmoothing module.
+        :param smoothing: label smoothing factor
+        """
+        super(LabelSmoothingCrossEntropy, self).__init__()
+        assert smoothing < 1.0
+        self.smoothing = smoothing
+        self.confidence = 1. - smoothing
+
+    def forward(self, x, target):
+        logprobs = F.log_softmax(x, dim=-1)
+        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
+        nll_loss = nll_loss.squeeze(1)
+        smooth_loss = -logprobs.mean(dim=-1)
+        loss = self.confidence * nll_loss + self.smoothing * smooth_loss
+        return loss.mean()
+
+class DenseCrossEntropy(nn.Module):
+    def forward(self, x, target):
+        x = x.float()
+        target = target.float()
+        logprobs = torch.nn.functional.log_softmax(x, dim=-1)
+
+        loss = -logprobs * target
+        loss = loss.sum(-1)
+        return loss.mean()
+
+class Cosface(nn.Module):
+    r"""Implement of large margin cosine distance: :
+    Args:
+        in_features: size of each input sample
+        out_features: size of each output sample
+        s: norm of input feature
+        m: margin
+        cos(theta) - m
+    """
+
+    def __init__(self, in_features, out_features, s=30.0, m=0.30):
+        super(Cosface, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.s = s
+        self.m = m
+        self.crit = DenseCrossEntropy()
+        # self.weight = Parameter(torch.FloatTensor(out_features, in_features).cuda())
+        
+        nn.init.xavier_uniform_(self.weight)
+
+    def forward(self, input, label):
+        # print(F.normalize(self.weight).shape)
+        # print(F.normalize(input).shape)
+
+        # --------------------------- cos(theta) & phi(theta) ---------------------------
+        # cosine = F.linear(F.normalize(input), F.normalize(self.weight))
+        cosine = input.float()
+        phi = cosine - self.m
+        # --------------------------- convert label to one-hot ---------------------------
+        one_hot = torch.zeros(cosine.size(), device='cuda')
+        # one_hot = one_hot.cuda() if cosine.is_cuda else one_hot
+        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is 0.4
+        output *= self.s
+        loss = self.crit(output, one_hot)
+        return loss
+        # print(output)
+
+        # return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' \
+               + 'in_features=' + str(self.in_features) \
+               + ', out_features=' + str(self.out_features) \
+               + ', s=' + str(self.s) \
+               + ', m=' + str(self.m) + ')'
+
+
+class AMSoftmax(nn.Module):
+    def __init__(self, in_features, out_features, s=30.0, m=0.30):
+        super(AMSoftmax, self).__init__()
+        self.m = m
+        self.s = s
+        self.in_feats = in_features
+        self.W = torch.nn.Parameter(torch.randn(in_features, out_features), requires_grad=True)
+        self.ce = nn.CrossEntropyLoss()
+        nn.init.xavier_normal_(self.W, gain=1)
+
+    def forward(self, x, lb):
+        assert x.size()[0] == lb.size()[0]
+        assert x.size()[1] == self.in_feats
+        x_norm = torch.norm(x, p=2, dim=1, keepdim=True).clamp(min=1e-12)
+        x_norm = torch.div(x, x_norm)
+        w_norm = torch.norm(self.W, p=2, dim=0, keepdim=True).clamp(min=1e-12)
+        w_norm = torch.div(self.W, w_norm)
+        costh = torch.mm(x_norm, w_norm)
+        # print(x_norm.shape, w_norm.shape, costh.shape)
+        lb_view = lb.view(-1, 1)
+        delt_costh = torch.zeros(costh.size(), device='cuda').scatter_(1, lb_view, self.m)
+        costh_m = costh - delt_costh
+        costh_m_s = self.s * costh_m
+        return costh_m_s
+
+class CenterLoss(nn.Module):
+    """Center loss.
+    Reference:
+    Wen et al. A Discriminative Feature Learning Approach for Deep Face Recognition. ECCV 2016.
+    Args:
+        num_classes (int): number of classes.
+        feat_dim (int): feature dimension.
+    """
+
+    def __init__(self, num_classes=751, feat_dim=2048, use_gpu=True):
+        super(CenterLoss, self).__init__()
+        self.num_classes = num_classes
+        self.feat_dim = feat_dim
+        self.use_gpu = use_gpu
+
+        if self.use_gpu:
+            self.centers = nn.Parameter(torch.randn(self.feat_dim, self.num_classes ).cuda())
+        else:
+            self.centers = nn.Parameter(torch.randn(self.feat_dim, self.num_classes))
+
+    def forward(self, x, labels):
+        """
+        Args:
+            x: feature matrix with shape (batch_size, feat_dim).
+            labels: ground truth labels with shape (num_classes).
+        """
+        assert x.size(0) == labels.size(0), "features.size(0) is not equal to labels.size(0)"
+        batch_size = x.size(0)
+        distmat = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
+                  torch.pow(self.centers, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes)
+        print(x.shape, labels.shape)
+        print(self.centers.t().shape,distmat.shape)
+
+        distmat.addmm_(x, self.centers.t(), beta = 1, alpha=-2, )
+
+        classes = torch.arange(self.num_classes).long()
+        if self.use_gpu: classes = classes.cuda()
+        labels = labels.unsqueeze(1).expand(batch_size, self.num_classes)
+        mask = labels.eq(classes.expand(batch_size, self.num_classes))
+
+        dist = []
+        for i in range(batch_size):
+            value = distmat[i][mask[i]]
+            value = value.clamp(min=1e-12, max=1e+12)  # for numerical stability
+            dist.append(value)
+        dist = torch.cat(dist)
+        loss = dist.mean()
+        return loss
